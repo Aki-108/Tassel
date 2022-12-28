49294a271f6862194711bb7251623e2769674193
Aki <75360508+Aki-108@users.noreply.github.com>
2022-12-28 07:08:07 +0100

    Create extensionsIndex.js

---
+++ extensionsIndex.js
@@ -0,0 +1,44 @@
+let extensionsIndex = [
+        {
+            "name": "Lightbox",
+            "id": 1,
+            "version": "1.3",
+            "description": "Adds a lightbox to Pillowfort.social.",
+            "author": "aki108",
+            "src": "https://cdn.rawgit.com/Aki-108/pf-lightbox/main/main.js"
+        },
+        {
+            "name": "Post Subscriber",
+            "id": 2,
+            "version": "1.3",
+            "description": "Get notified when there are new comments in a post.",
+            "author": "aki108",
+            "src": "https://cdn.rawgit.com/Aki-108/pf-post-subscriber/main/main.js"
+        },
+        {
+            "name": "Reblogged to Community",
+            "id": 3,
+            "version": "2.0",
+            "description": "Shows where a post has been liked/reblogged to.",
+            "author": "aki108",
+            "src": "https://cdn.rawgit.com/Aki-108/pf-reblogged-to-community/main/main.js"
+        },
+        {
+            "name": "Advanced Blacklist",
+            "id": 4,
+            "version": "0.0",
+            "author": "aki108"
+        },
+        {
+            "name": "Read More Inplace",
+            "id": 5,
+            "version": "0.0",
+            "author": "aki108"
+        },
+        {
+            "name": "User Info",
+            "id": 6,
+            "version": "0.0",
+            "author": "aki108"
+        }
+    ];
