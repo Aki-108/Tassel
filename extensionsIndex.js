fdd1d43fb3affa74240fa348c40996f03e10b3cd
Aki <75360508+Aki-108@users.noreply.github.com>
2022-12-29 11:03:49 +0100

    Update extensionsIndex.js
    • added "created" date
    • added "updated" date

--- extensionsIndex.js
+++ extensionsIndex.js
@@ -3,6 +3,8 @@ let extensionsIndex = [
             "name": "Lightbox",
             "id": 1,
             "version": "1.3",
+            "created": 1660385280000,
+            "updated": 1664909460000,
             "description": "Adds a lightbox to Pillowfort.social.",
             "author": "aki108",
             "src": "https://cdn.rawgit.com/Aki-108/pf-lightbox/main/main.js"
@@ -11,6 +13,8 @@ let extensionsIndex = [
             "name": "Post Subscriber",
             "id": 2,
             "version": "1.3",
+            "created": 1661764440000,
+            "updated": 1668446760000,
             "description": "Get notified when there are new comments in a post.",
             "author": "aki108",
             "src": "https://cdn.rawgit.com/Aki-108/pf-post-subscriber/main/main.js"
@@ -19,6 +23,8 @@ let extensionsIndex = [
             "name": "Reblogged to Community",
             "id": 3,
             "version": "2.0",
+            "created": 1653984960000,
+            "updated": 1670166000000,
             "description": "Shows where a post has been liked/reblogged to.",
             "author": "aki108",
             "src": "https://cdn.rawgit.com/Aki-108/pf-reblogged-to-community/main/main.js"
@@ -49,6 +55,8 @@ let extensionsIndex = [
     "name": "",
     "id": 0,
     "version": "",
+    "created": 0,
+    "updated": 0,
     "author": "",
     "icon": "",
     "description": "",
