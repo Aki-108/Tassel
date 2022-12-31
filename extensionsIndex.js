6180640cef638a02bc1f5231ddfcc544b2c6dd8c
Aki <75360508+Aki-108@users.noreply.github.com>
2022-12-31 10:17:22 +0100

    Update extensionsIndex.js
    • added more details
    • changed cached URLs to include the version

--- extensionsIndex.js
+++ extensionsIndex.js
@@ -5,9 +5,18 @@ let extensionsIndex = [
             "version": "1.3",
             "created": 1660385280000,
             "updated": 1664909460000,
-            "description": "Adds a lightbox to Pillowfort.social.",
             "author": "aki108",
-            "src": "https://cdn.rawgit.com/Aki-108/pf-lightbox/main/main.js"
+            "description": "View Images in Full Screen",
+            "src": "https://cdn.rawgit.com/Aki-108/pf-lightbox/9c022cb26bda5ac2ec57ef3179c1edde6d18ff69/main.js",
+            "post": "https://www.pillowfort.social/posts/2841345",
+            "features": [
+                {
+                        "name": "+ view images in full screen"
+                },
+                {
+                        "name": "+ dedicated button for embedded links"
+                }
+            ]
         },
         {
             "name": "Post Subscriber",
@@ -15,9 +24,21 @@ let extensionsIndex = [
             "version": "1.3",
             "created": 1661764440000,
             "updated": 1668446760000,
-            "description": "Get notified when there are new comments in a post.",
             "author": "aki108",
-            "src": "https://cdn.rawgit.com/Aki-108/pf-post-subscriber/main/main.js"
+            "description": "Get notified when there are new comments in a post.",
+            "src": "https://cdn.rawgit.com/Aki-108/pf-post-subscriber/08385e8dba54ce5a9a06e2de64aa1a5312b54c0a/main.js",
+            "post": "https://www.pillowfort.social/posts/2878877",
+            "features": [
+                {
+                        "name": "+ subscribe to posts"
+                },
+                {
+                        "name": "+ notifications for new comments in the sidebar"
+                },
+                {
+                        "name": "+ highlight new comments"
+                }
+            ]
         },
         {
             "name": "Reblogged to Community",
@@ -25,14 +46,26 @@ let extensionsIndex = [
             "version": "2.0",
             "created": 1653984960000,
             "updated": 1670166000000,
-            "description": "Shows where a post has been liked/reblogged to.",
             "author": "aki108",
-            "src": "https://cdn.rawgit.com/Aki-108/pf-reblogged-to-community/main/main.js"
+            "description": "Shows where a post has been liked/reblogged to.",
+            "src": "https://cdn.rawgit.com/Aki-108/pf-reblogged-to-community/b287c3cb88ae9cb65280aef1cd3b7c75eb7d5713/main.js",
+            "post": "https://www.pillowfort.social/posts/3057985",
+            "features": [
+                {
+                        "name": "+ show where a post has been reblogged to"
+                },
+                {
+                        "name": "+ show where a post has been liked from"
+                },
+                {
+                        "name": "* makes notes actually dark in dark mode"
+                }
+            ]
         },
         {
             "name": "Advanced Blacklist",
             "id": 4,
-            "version": "0.0",
+            "version": "0.1",
             "author": "aki108"
         },
         {
