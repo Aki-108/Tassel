4d4f0dde4f2af9903b31dc93c74ad9a111a290d2
Aki <75360508+Aki-108@users.noreply.github.com>
2023-01-01 11:57:17 +0100

    Update extensionsIndex.js

--- extensionsIndex.js
+++ extensionsIndex.js
@@ -36,7 +36,7 @@ let extensionsIndex = [
             "updated": 1672504980000,
             "author": "aki108",
             "description": "Shows where a post has been liked/reblogged to.",
-            "src": "https://cdn.rawgit.com/Aki-108/Tassel/bf4b70c41f04ee4dcb17593ab08f4990eedc1541/extensions/RebloggedToCommunity.js",
+            "src": "https://cdn.rawgit.com/Aki-108/Tassel/af093ad2ee843fd57c428246bbcec991cadad85e/extensions/RebloggedToCommunity.js",
             "post": "https://www.pillowfort.social/posts/3057985",
             "features": ["+ show where a post has been reblogged to",
                          "+ show where a post has been liked from",
