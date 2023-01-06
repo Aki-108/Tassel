4924e0d91b9f8b7f1c0b4c26cfc5f4c12199e854
Aki <75360508+Aki-108@users.noreply.github.com>
2023-01-06 20:12:35 +0100

    Update toasts.js

--- toasts.js
+++ toasts.js
@@ -3,7 +3,7 @@ let toasts = [
     "id": 0,
     "extension": 0,
     "title": "Thanks for using Tassel",
-    "text": "Click on the Tassel button in the sidebar to start using extensions.",
+    "text": "Click on the Tassel button in the sidebar to start using extensions. Click this message to hide it.",
     "timestamp": 0,
     "new": false
   }
