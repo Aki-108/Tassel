e4c40ee65f1e7acd03342c15b5d833fe5d15ed50
Aki <75360508+Aki-108@users.noreply.github.com>
2023-01-29 21:23:55 +0100

    Update tassel.js

--- tassel.js
+++ tassel.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Tassel
-// @version      1.2.2
+// @version      1.2.3
 // @description  Pillowfort Extension Manager. Makes the use of a variety of extensions easier.
 // @author       aki108
 // @match        http*://www.pillowfort.social/*
@@ -16,7 +16,7 @@
 (function() {
     'use strict';
 
-    let extensionsIndexURL = "https://cdn.rawgit.com/Aki-108/Tassel/c9a8a0415532f881038f4811215032606f781ed7/extensionsIndex.js";
+    let extensionsIndexURL = "https://cdn.rawgit.com/Aki-108/Tassel/f49b40e78c22fa6cfa506fa03be7a51fa346a512/extensionsIndex.js";
     let toastsURL = "https://cdn.rawgit.com/Aki-108/Tassel/4253d4d5f2692603ce236238e5e04c9e9befbf3e/toasts.js";
 
     let icon = document.createElement("div");
