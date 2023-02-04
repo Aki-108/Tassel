af310a1a71f7e691b507b0019a1877f70fb2007c
Aki <75360508+Aki-108@users.noreply.github.com>
2023-02-04 20:24:17 +0100

    Update BlocklistAnnotations.js

--- extensions/BlocklistAnnotations.js
+++ extensions/BlocklistAnnotations.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Blocklist Annotations
-// @version      1.0
+// @version      1.1
 // @description  Write down why you blocked someone.
 // @author       aki108
 // @match        https://www.pillowfort.social/*
@@ -14,7 +14,7 @@
     if (window.location.pathname !== "/block_list") return;
 
     // list of annotations
-    let annotations = JSON.parse(localStorage.getItem("blocklistAnnotations")) || [];
+    let annotations = (JSON.parse(localStorage.getItem("tasselBlocklistAnnotations")) || {"annotations": []}).annotations;
 
     // create input fields
     let rows = document.getElementsByClassName("block-row");
@@ -22,14 +22,20 @@
         el.setAttribute('style', 'display:grid; grid-template-columns:auto 200px 90px;grid-column-gap:10px;');
 
         let input = document.createElement("textarea");
+        input.setAttribute("rows", 1);
         input.placeholder = "Annotation";
         input.addEventListener("blur", function() {
             let data = {};
             data.user = this.parentNode.children[0].children[1].innerHTML;
             data.note = this.value;
+            for (let index in annotations) {
+                let item = annotations[index];
+                if (item.user !== data.user) continue;
+                annotations.splice(index, 1);
+            }
             if (this.value !== "") {
                 annotations.push(data);
-                localStorage.setItem("blocklistAnnotations", JSON.stringify(annotations));
+                localStorage.setItem("tasselBlocklistAnnotations", JSON.stringify({"annotations": annotations}));
             }
         });
         el.insertBefore(input, el.getElementsByClassName("unblock-btn-container")[0]);
