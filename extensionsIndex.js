7e043da5b410f5acd6b8f596bd251d943f92c942
Aki <75360508+Aki-108@users.noreply.github.com>
2023-04-01 17:05:09 +0200

    Updated URLs

--- extensionsIndex.js
+++ extensionsIndex.js
@@ -7,7 +7,7 @@ let extensionsIndex = [
             "updated": 1676186820000,
             "author": "Aki108",
             "description": "View images in full screen",
-            "src": "https://cdn.rawgit.com/Aki-108/Tassel/0b19d974426f3154896fa6447efc58f471b77355/extensions/Lightbox.js",
+            "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@0b19d974426f3154896fa6447efc58f471b77355/extensions/Lightbox.js",
             "post": "https://www.pillowfort.social/posts/2841345",
             "features": ["View images in full screen.",
                          "Adds a dedicated button for embedded links."
@@ -21,7 +21,7 @@ let extensionsIndex = [
             "updated": 1676196720000,
             "author": "Aki108",
             "description": "Get notified when there are new comments in a post.",
-            "src": "https://cdn.rawgit.com/Aki-108/Tassel/6630ccab2b0b19ffc32399519530b234e77d2ac0/extensions/PostSubscriber.js",
+            "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@6630ccab2b0b19ffc32399519530b234e77d2ac0/extensions/PostSubscriber.js",
             "post": "https://www.pillowfort.social/posts/2878877",
             "features": ["Subscribe to posts.",
                          "Get notifications for new comments in the sidebar.",
@@ -36,7 +36,7 @@ let extensionsIndex = [
             "updated": 1672861800000,
             "author": "Aki108",
             "description": "Shows where a post has been liked/reblogged to.",
-            "src": "https://cdn.rawgit.com/Aki-108/Tassel/aeafb71abc67a4bb06b7f4488df63acbb1aee066/extensions/RebloggedToCommunity.js",
+            "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@aeafb71abc67a4bb06b7f4488df63acbb1aee066/extensions/RebloggedToCommunity.js",
             "post": "https://www.pillowfort.social/posts/3057985",
             "features": ["Show where a post has been reblogged to.",
                          "Show where a post has been liked from.",
@@ -51,7 +51,7 @@ let extensionsIndex = [
             "updated": 1677918660000,
             "author": "aki108",
             "description": "New and improved blacklist with lots of settings.",
-            "src": "https://cdn.rawgit.com/Aki-108/Tassel/0a929b1c1bb3bc18df13a8c5fd74c3b93c2dc393/extensions/AdvancedBlacklist.js",
+            "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@0a929b1c1bb3bc18df13a8c5fd74c3b93c2dc393/extensions/AdvancedBlacklist.js",
             "post": "https://www.pillowfort.social/posts/3215881",
             "features": [
                 "Block posts by tag, content, username or community.",
@@ -67,7 +67,7 @@ let extensionsIndex = [
             "updated": 1676147520000,
             "author": "Aki108",
             "description": "Collapse comments and threads.",
-            "src": "https://cdn.rawgit.com/Aki-108/Tassel/74ace6a11a2ee0a6595bda6071583eaa259a42f0/extensions/CollapsibleThreads.js",
+            "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@74ace6a11a2ee0a6595bda6071583eaa259a42f0/extensions/CollapsibleThreads.js",
             "post": "https://www.pillowfort.social/posts/3161577"
         },
         {
@@ -78,7 +78,7 @@ let extensionsIndex = [
             "updated": 1675538640000,
             "author": "Aki108",
             "description": "Remember the reason you blocked someone.",
-            "src": "https://cdn.rawgit.com/Aki-108/Tassel/af310a1a71f7e691b507b0019a1877f70fb2007c/extensions/BlocklistAnnotations.js",
+            "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@af310a1a71f7e691b507b0019a1877f70fb2007c/extensions/BlocklistAnnotations.js",
             "post": "https://www.pillowfort.social/posts/3167192"
         }
     ];
