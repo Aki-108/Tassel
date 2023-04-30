96b23523a6c69dc364bf2eace83207be79d1f67e
Aki <75360508+Aki-108@users.noreply.github.com>
2023-04-30 16:48:48 +0200

    Update toasts.js

--- toasts.js
+++ toasts.js
@@ -8,38 +8,6 @@ let toasts = [
     "new": false
   },
   {
-    "id": 2,
-    "extension": 6,
-    "title": "New Extension",
-    "text": "Blocklist Annotations is now available for activation.",
-    "timestamp": 1676199117382,
-    "new": true
-  },
-  {
-    "id": 3,
-    "extension": 7,
-    "title": "New Extension",
-    "text": "Tagging Tools is now available for activation.",
-    "timestamp": 1678386060000,
-    "new": true
-  },
-  {
-    "id": 4,
-    "extension": 2,
-    "title": "Bug Fix",
-    "text": "Post Subscriber has undergone several bug fixes.",
-    "timestamp": 1678476223662,
-    "new": false
-  },
-  {
-    "id": 5,
-    "extension": 7,
-    "title": "Tagging Tools: Bug Fix",
-    "text": "Styling loads now.",
-    "timestamp": 1679751117531,
-    "new": false
-  },
-  {
     "id": 6,
     "extension": 4,
     "title": "Advanced Blacklist",
@@ -53,6 +21,14 @@ let toasts = [
     "title": "Tassel Update",
     "text": "New Appearance Settings have been added to Tassel.",
     "timestamp": 1680855960000,
+    "new": false
+  },
+  {
+    "id": 8,
+    "extension": 3,
+    "title": "Reblogged To Community",
+    "text": "There's a new option to show the tags of a reblog in the post notes. Turn it on in the settings.",
+    "timestamp": 1682865300000,
     "new": false
   }
 ];
