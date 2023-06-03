dda7e6f09f49ca598d8cb76c358a9cec60992da4
Aki <75360508+Aki-108@users.noreply.github.com>
2023-06-03 15:30:32 +0200

    Post Subscriber Redesign

--- toasts.js
+++ toasts.js
@@ -8,35 +8,19 @@ let toasts = [
     "new": false
   },
   {
-    "id": 6,
-    "extension": 4,
-    "title": "Advanced Blacklist",
-    "text": "Advanced Blacklist is now available as a full release.",
-    "timestamp": 1680855960000,
-    "new": true
-  },
-  {
-    "id": 7,
-    "extension": 0,
-    "title": "Tassel Update",
-    "text": "New Appearance Settings have been added to Tassel.",
-    "timestamp": 1680855960000,
-    "new": false
-  },
-  {
-    "id": 8,
-    "extension": 3,
-    "title": "Reblogged To Community",
-    "text": "There's a new option to show the tags of a reblog in the post notes. Turn it on in the settings.",
-    "timestamp": 1682865300000,
-    "new": false
-  },
-  {
     "id": 9,
     "extension": 7,
     "title": "New Features",
     "text": "Tagging Tools now has settings for default tags and auto-copying on reblogs.",
     "timestamp": 1683375780000,
+    "new": false
+  },
+  {
+    "id": 10,
+    "extension": 2,
+    "title": "Post Subscriber",
+    "text": "got a new design.",
+    "timestamp": 1685798654162,
     "new": false
   }
 ];
