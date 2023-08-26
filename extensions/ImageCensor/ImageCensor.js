c8f3010831253743bc205024fdf4c378663d465b
Aki <75360508+Aki-108@users.noreply.github.com>
2023-08-26 17:02:29 +0200

    Commented

--- extensions/ImageCensor/ImageCensor.js
+++ extensions/ImageCensor/ImageCensor.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Image Censor
-// @version      0.2
+// @version      1.0
 // @description  Censor (NSFW) images and icons.
 // @author       aki108
 // @match        https://www.pillowfort.social/*
@@ -11,56 +11,72 @@
 (function() {
     'use strict';
 
-    let settings = JSON.parse(localStorage.getItem("tasselSettings2")).imageCensor || {};
+    let settings = JSON.parse(localStorage.getItem("tasselSettings2")).imageCensor || {blurNSFW: true};
     if (!settings.censorUsers) settings.censorUsers = [];
 
-    initTassel();
+    /* Initialize */
+    initTassel_kyhjxbvr();
     document.getElementById("tasselJsonManagerModalReady").addEventListener("click", function() {
-        processPosts([tasselJsonManager.modal.json]);
+        processPosts_kyhjxbvr([tasselJsonManager.modal.json]);
     });
     document.getElementById("tasselJsonManagerPostReady").addEventListener("click", function() {
-        processPosts([tasselJsonManager.post.json]);
+        processPosts_kyhjxbvr([tasselJsonManager.post.json]);
     });
     document.getElementById("tasselJsonManagerFeedReady").addEventListener("click", function() {
-        processPosts(tasselJsonManager.feed.posts);
+        processPosts_kyhjxbvr(tasselJsonManager.feed.posts);
     });
     document.getElementById("tasselJsonManagerCommentReady").addEventListener("click", function() {
-        censorCommentIcon(tasselJsonManager.comments.comments);
+        censorCommentIcon_kyhjxbvr(tasselJsonManager.comments.comments);
     });
 
-    function processPosts(postData) {
+    /* Loop through every post */
+    function processPosts_kyhjxbvr(postData) {
+        //get HTML elements for posts
         let links = Object.values(document.getElementsByClassName("link_post"));
         links = links.filter(function(item) {
             return !item.classList.contains("tasselPermalinked");
         });
 
+        //loop through data
         postData.forEach(function(post) {
+            //match data with an HTML element
             let postElement = links.filter(function(item) {
                 if (!item.href) return;
                 return item.href.split("/")[4] == (post.original_post_id || post.id);
             });
             if (!links.length) postElement = Object.values(document.getElementsByClassName("post-container"));
+
+            //call changing functions on that element
             postElement.forEach(function(postEl) {
+                //get root element
                 for (let a = 0; a < 100; a++, postEl = postEl.parentNode) {
                     if (postEl.classList.contains("post-container")) break;
                 }
                 if (postEl.classList.contains("tasselImageCensorProcessed")) return;
                 postEl.classList.add("tasselImageCensorProcessed");
 
-                if (post.nsfw && settings.blurNSFW || settings.blurAny) blurImage(postEl);
-                if (post.nsfw && settings.collapseNSFW) collapsePost(postEl);
+                //blur images
+                if (post.nsfw && settings.blurNSFW || settings.blurAny) blurImage_kyhjxbvr(postEl);
 
+                //collapse posts
+                if (post.nsfw && settings.collapseNSFW) collapsePost_kyhjxbvr(postEl);
+
+                //censor icons
                 let username = post.original_username || post.username;
-                if (settings.censorUsers.includes(username)) censorIcon(postEl);
+                if (settings.censorUsers.includes(username)) censorIcon_kyhjxbvr(postEl);
             });
         });
     };
 
-    function blurImage(postElement) {
+    /* Apply the blur-class to every image in a post */
+    function blurImage_kyhjxbvr(postElement) {
+        //for photo-posts
         let images = postElement.getElementsByClassName("media")[0];
         if (images) Object.values(images.children).forEach(function(item) {
             item.classList.add("tasselNsfwBlur");
         });
+
+        //for the text-body
         let textBody = postElement.getElementsByClassName("content")[0];
         if (textBody) textBody = textBody.getElementsByTagName("img");
         if (textBody) Object.values(textBody).forEach(function(item) {
@@ -68,13 +84,16 @@
         });
     }
 
-    function collapsePost(postElement) {
+    /* Collapse posts */
+    function collapsePost_kyhjxbvr(postElement) {
+        //use this in combination with Advanced Blacklist
         let container = postElement.getElementsByClassName("tasselAdvancedBlacklistBlockedHeader")[0];
         if (container) {
             container.children[1].innerHTML += "<br>This post is marked as NSFW.";
             return;
         }
 
+        //use this without Advanced Blacklist
         container = document.createElement("div");
         container.classList.add("tasselAdvancedBlacklistBlockedHeader");
         container.innerHTML = `
@@ -100,14 +119,18 @@
         for (let el of postElement.getElementsByClassName("advancedBlacklistHidden")) el.style.display = "none";
     }
 
-    function censorIcon(postElement) {
+    /* Replace user icon with the default icon */
+    function censorIcon_kyhjxbvr(postElement) {
         postElement.getElementsByClassName("avatar")[0].children[0].src = "https://img3.pillowfort.social/pf-default-user.png";
     }
 
-    function censorCommentIcon(comments) {
+    /* Replace user icon with the default icon */
+    function censorCommentIcon_kyhjxbvr(comments) {
         let nameElements = Object.values(document.getElementsByClassName("comment-title"));
+
         comments.forEach(function(comment) {
             if (!settings.censorUsers.includes(comment.username)) return;
+
             let elements = nameElements.filter(function(item) {
                 return item.innerHTML === comment.username;
             });
@@ -121,7 +144,7 @@
     }
 
     /* Add elements to the Tassel menu */
-    function initTassel() {
+    function initTassel_kyhjxbvr() {
         let tasselSidebar = document.getElementById("tasselModalSidebar");
         if (!tasselSidebar) return;
         let button = document.createElement("button");
@@ -129,11 +152,11 @@
         button.id = "tasselModalSidebarImageCensor";
         button.innerHTML = "Image Censor";
         tasselSidebar.appendChild(button);
-        document.getElementById("tasselModalSidebarImageCensor").addEventListener("click", displaySettings);
+        document.getElementById("tasselModalSidebarImageCensor").addEventListener("click", displaySettings_kyhjxbvr);
     }
 
     /* Create Tassel settings menu */
-    function displaySettings() {
+    function displaySettings_kyhjxbvr() {
         //deselect other menu items and select this one
         let content = document.getElementById("tasselModalContent");
         content.innerHTML = "";
@@ -150,27 +173,27 @@
         content.appendChild(document.createElement("hr"));
 
         //add settings
-        content.appendChild(createSwitch_dshcgkhy("Blur NSFW images", settings.blurNSFW ? "checked" : ""));
+        content.appendChild(createSwitch_kyhjxbvr("Blur NSFW images", settings.blurNSFW ? "checked" : ""));
         content.lastChild.children[0].addEventListener("change", function() {
             settings.blurNSFW = this.checked;
-            saveSettings();
+            saveSettings_kyhjxbvr();
         });
 
-        content.appendChild(createSwitch_dshcgkhy("Collapse NSFW posts", settings.collapseNSFW ? "checked" : ""));
+        content.appendChild(createSwitch_kyhjxbvr("Collapse NSFW posts", settings.collapseNSFW ? "checked" : ""));
         content.lastChild.children[0].addEventListener("change", function() {
             settings.collapseNSFW = this.checked;
-            saveSettings();
+            saveSettings_kyhjxbvr();
         });
 
-        content.appendChild(createSwitch_dshcgkhy("Blur any images", settings.blurAny ? "checked" : ""));
+        content.appendChild(createSwitch_kyhjxbvr("Blur any images", settings.blurAny ? "checked" : ""));
         content.lastChild.children[0].addEventListener("change", function() {
             settings.blurAny = this.checked;
-            saveSettings();
+            saveSettings_kyhjxbvr();
         });
         content.appendChild(document.createElement("hr"));
 
         let info4 = document.createElement("p");
-        info4.innerHTML = "Enter a list of users whose profile-picture should be blurred. One username per line."
+        info4.innerHTML = "Enter a list of users whose profile-picture should be censored. One username per line."
         content.appendChild(info4);
         let userInput = document.createElement("textarea");
         userInput.id = "tasselImageCensorUsers";
@@ -190,19 +213,19 @@
         button4.innerHTML = "Save";
         button4.addEventListener("click", function() {
             settings.censorUsers = document.getElementById("tasselImageCensorUsers").value.split("\n");
-            saveSettings();
+            saveSettings_kyhjxbvr();
         });
         frame4.appendChild(button4);
         content.appendChild(frame4);;
     }
 
-    function saveSettings() {
+    function saveSettings_kyhjxbvr() {
         let file = JSON.parse(localStorage.getItem("tasselSettings2") || "{}");
         file.imageCensor = settings;
         localStorage.setItem("tasselSettings2", JSON.stringify(file));
     }
 
-    function createSwitch_dshcgkhy(title="", state="") {
+    function createSwitch_kyhjxbvr(title="", state="") {
         let id = "tasselSwitch" + Math.random();
         let toggle = document.createElement("div");
         toggle.classList.add("tasselToggle");
