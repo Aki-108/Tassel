a47b756980cf5697bd635cd03252b7368815dc44
Aki <75360508+Aki-108@users.noreply.github.com>
2023-09-10 10:37:59 +0200

    Updated Post Subscriber Styling

--- extensionsIndex.js
+++ extensionsIndex.js
@@ -22,7 +22,7 @@ let extensionsIndex = [
             "author": "Aki108",
             "description": "Get notified when there are new comments on a post.",
             "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@219c75536ac76987cec15efe8829e32e70d6204a/extensions/PostSubscriber/PostSubscriber.js",
-            "css": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@8479f9ce5c5e71f88a1533afd67f3d5b98c14b96/extensions/PostSubscriber/PostSubscriber.css",
+            "css": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@fd0066832ce3fc6e629d3a3f3d4c54b7583fd8ed/extensions/PostSubscriber/PostSubscriber.css",
             "post": "https://www.pillowfort.social/posts/2878877",
             "features": ["Subscribe to posts.",
                          "Get notifications for new comments in the sidebar.",
