399a6546cbdda79069e3c650476e935eaa63099a
Aki <75360508+Aki-108@users.noreply.github.com>
2023-09-30 19:54:48 +0200

    Added Settings

--- extensions/ReadThis/ReadThis.js
+++ extensions/ReadThis/ReadThis.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Read This
-// @version      0.2
+// @version      0.3
 // @description  Open read-more's anywhere.
 // @author       Aki108
 // @match        https://www.pillowfort.social/*
@@ -11,34 +11,39 @@
 (function() {
     'use strict';
 
-    let maxHeight = 200;
+    let settings = JSON.parse(localStorage.getItem("tasselSettings2")).readThis || {maxHeight: 800, shortHeight: 500};
 
     /* Initialize */
+    initTassel_icigqyni();
     document.getElementById("tasselJsonManagerModalReady").addEventListener("click", function() {
+        //undo changes from previous modal opening
         let links = Object.values(document.getElementById("post-view-modal").getElementsByClassName("link_post"));
-        let originalView = document.getElementById("post-view-modal").getElementsByClassName("tasselReadMoreOldPost");
+        let originalView = document.getElementById("post-view-modal").getElementsByClassName("tasselReadThisOldPost");
         if (originalView.length) {
-            originalView[0].classList.remove("tasselReadMoreOldPost");
+            originalView[0].classList.remove("tasselReadThisOldPost");
             document.getElementById("post-view-modal").getElementsByClassName("post-container")[0].classList.remove("tasselReadThisProcessed");
-            document.getElementById("post-view-modal").getElementsByClassName("tasselReadMoreNewPost")[0].remove();
+            document.getElementById("post-view-modal").getElementsByClassName("tasselReadThisNewPost")[0].remove();
         }
-        processPosts([tasselJsonManager.modal.json], links);
+        processPosts_icigqyni([tasselJsonManager.modal.json], links);
     });
     document.getElementById("tasselJsonManagerFeedReady").addEventListener("click", function() {
+        //get HTML elements for posts
         let links = Object.values(document.getElementsByClassName("link_post"));
         links = links.filter(function(item) {
             return !item.classList.contains("tasselPermalinked");
         });
-        processPosts(tasselJsonManager.feed.posts, links);
+        processPosts_icigqyni(tasselJsonManager.feed.posts, links);
     });
 
-    function processPosts(posts, links) {//get HTML elements for posts
-        addDetails(posts, links);
-        shortenPosts(links);
+    function processPosts_icigqyni(posts, links) {
+        addDetails_icigqyni(posts, links);
+        shortenPosts_icigqyni(links);
     }
 
-    function addDetails(posts, links) {
+    /* Turn READ-MORE's into collapsable detail-elements */
+    function addDetails_icigqyni(posts, links) {
         posts.forEach(function(post) {
+            //exit when there is no READ-MORE in the post
             if (post.content.indexOf("[READ-MORE]") === -1) return;
 
             //match data with an HTML element
@@ -49,68 +54,147 @@
             if (!links.length) postElement = Object.values(document.getElementsByClassName("post-container"));
 
             postElement.forEach(function(postEl) {
-                for (let a = 0; a < 100; a++, postEl = postEl.parentNode) {//get root element
+                //get root element
+                for (let a = 0; a < 100; a++, postEl = postEl.parentNode) {
                     if (postEl.classList.contains("post-container")) break;
                 }
                 if (postEl.classList.contains("tasselReadThisProcessed")) return;
                 postEl.classList.add("tasselReadThisProcessed");
 
+                //create a new text-body without READ-MORE's
                 let fullPost = document.createElement("div");
-                fullPost.classList.add("tasselReadMoreNewPost");
-                let contentFormated = post.content.replaceAll("[READ-MORE]", "<details class='tasselReadMoreNewBlock'><summary>Read This...</summary>");
+                fullPost.classList.add("tasselReadThisNewPost");
+                let contentFormated = post.content.replaceAll("[READ-MORE]", "<details class='tasselReadThisNewBlock'><summary>Read This...</summary>");
                 contentFormated = contentFormated.replaceAll("[/READ-MORE]", "</details>");
                 fullPost.innerHTML = contentFormated;
 
                 let content = postEl.getElementsByClassName("content")[0].children[0];
-                content.classList.add("tasselReadMoreOldPost");
+                content.classList.add("tasselReadThisOldPost");
                 content.after(fullPost);
             });
         });
     }
 
-    function shortenPosts(links) {
+    /* Cutdown long posts */
+    function shortenPosts_icigqyni(links) {
         links.forEach(function(link) {
+            //get root element
             let postElement = link;
-            for (let a = 0; a < 100; a++, postElement = postElement.parentNode) {//get root element
+            for (let a = 0; a < 100; a++, postElement = postElement.parentNode) {
                 if (postElement.classList.contains("post-container")) break;
             }
             if (postElement.classList.contains("tasselReadThisProcessed2")) return;
             postElement.classList.add("tasselReadThisProcessed2");
 
-            let content = postElement.getElementsByClassName("content")[0];
-            let contentHeight = content.getClientRects();
-            contentHeight = contentHeight.length ? contentHeight = contentHeight[0].height : contentHeight = 0;
-            let media = postElement.getElementsByClassName("media");
-            media = media.length ? media[0] : null
-            let mediaHeight = media && media.style.display !== "none" ? media.getClientRects()[0].height : 0;
+            //find text-body
+            let content = postElement.getElementsByClassName("content");
+            if (content.length) content = content[0];
+            else content = null;
 
+            let contentHeight = 0;
+            if (content) contentHeight = content.getBoundingClientRect().height;
+
+            //find media-body (images, embeds, ...)
+            let media = postElement.getElementsByClassName("media");
+            if (media.length) media = media[0];
+            else media = null;
+
+            let mediaHeight = 0;
+            if (media && media.style.display !== "none") mediaHeight = media.getBoundingClientRect().height;
+
+            //exit if the post is short or collapsed
+            if (contentHeight + mediaHeight < settings.maxLength) return;
+            if (content && content.classList.contains("advancedBlacklistHidden")) return;
+
+            //add a button to open shortened posts
             let button = document.createElement("div");
             button.innerHTML = `
-                <div style="height: 100px;margin-top:-99px;background:transparent;position: relative;background: linear-gradient(0deg, var(--postHeaderFooter) 0%, transparent 100%);"></div>
-                <div style="display: flex;justify-content: center;padding: .5em;background-color: var(--postHeaderFooter);">
-                    <button class="tempClass" style="background: none;border: none;color: var(--linkColor);">Read This</button>
+                <div class="tasselReadThisFade"></div>
+                <div class="tasselReadThisOpen">
+                    <button>Read This</button>
                 </div>
             `;
+            (content || media).after(button);
+            button.children[1].children[0].addEventListener("click", function() {
+                this.parentNode.parentNode.style.display = "none";
+                this.parentNode.parentNode.parentNode.getElementsByClassName("content")[0].style.maxHeight = "unset";
+                this.parentNode.parentNode.parentNode.getElementsByClassName("content")[0].style.display = "block";
+                let media = this.parentNode.parentNode.parentNode.getElementsByClassName("media");
+                if (media.length) media[0].style.maxHeight = "unset";
+            });
 
-            if (contentHeight + mediaHeight < maxHeight) return;
-            content.after(button);
-            content.classList.add("tasselReadThisShort");
-            let contentMaxHeight = Math.max(maxHeight - mediaHeight, 0);
-            if (contentMaxHeight > 0) content.style.maxHeight = contentMaxHeight + "px";
-            else content.style.display = "none";
+            //do the actual shortening
             if (media) {
                 media.classList.add("tasselReadThisShort");
-                media.style.maxHeight = maxHeight + "px";
+                media.style.maxHeight = settings.shortLength + "px";
             }
-
-            Object.values(document.getElementsByClassName("tempClass")).forEach(function(item) {
-                item.addEventListener("click", function() {
-                    this.parentNode.parentNode.style.display = "none";
-                    this.parentNode.parentNode.parentNode.getElementsByClassName("content")[0].style.maxHeight = "unset";
-                    this.parentNode.parentNode.parentNode.getElementsByClassName("content")[0].style.display = "block";
-                    this.parentNode.parentNode.parentNode.getElementsByClassName("media")[0].style.maxHeight = "unset";
-                });
-            });
+            if (content) {
+                content.classList.add("tasselReadThisShort");
+                let contentMaxHeight = Math.max(settings.shortLength - mediaHeight, 0);
+                if (contentMaxHeight > 0) content.style.maxHeight = contentMaxHeight + "px";
+                else content.style.display = "none";
+            }
         });
+    }
+
+    /* Add elements to the Tassel menu */
+    function initTassel_icigqyni() {
+        let tasselSidebar = document.getElementById("tasselModalSidebar");
+        if (!tasselSidebar) return;
+        let button = document.createElement("button");
+        button.classList.add("tasselModalSidebarEntry");
+        button.id = "tasselModalSidebarReadThis";
+        button.innerHTML = "Read This";
+        tasselSidebar.appendChild(button);
+        document.getElementById("tasselModalSidebarReadThis").addEventListener("click", displaySettings_icigqyni);
+    }
+
+    /* Create Tassel settings menu */
+    function displaySettings_icigqyni() {
+        //deselect other menu items and select this one
+        let content = document.getElementById("tasselModalContent");
+        content.innerHTML = "";
+        let sidebarEntries = document.getElementsByClassName("tasselModalSidebarEntry");
+        Object.values(sidebarEntries).forEach(function(data, index) {
+            data.classList.remove("active");
+        });
+        document.getElementById("tasselModalSidebarReadThis").classList.add("active");
+
+        //add a little note
+        let info0 = document.createElement("p");
+        info0.innerHTML = "Changes will become active after a page reload.";
+        content.appendChild(info0);
+        content.appendChild(document.createElement("hr"));
+
+        //add settings
+        let label1 = document.createElement("label");
+        label1.setAttribute("for", "tasselReadThisMaxLengthInput");
+        label1.innerHTML = `Shorten posts that are longer than
+            <input id="tasselReadThisMaxLengthInput" type="number" min="0" step="20" value="${settings.maxLength}"></input>
+            px.
+        `;
+        content.appendChild(label1);
+        content.lastChild.children[0].addEventListener("change", function() {
+            settings.maxLength = this.value;
+            saveSettings_icigqyni();
+        });
+
+        let label2 = document.createElement("label");
+        label2.setAttribute("for", "tasselReadThisShortLengthInput");
+        label2.innerHTML = `Shorten posts to
+            <input id="tasselReadThisShortLengthInput" type="number" min="0" step="20" value="${settings.shortLength}"></input>
+            px.
+        `;
+        content.appendChild(label2);
+        content.lastChild.children[0].addEventListener("change", function() {
+            settings.shortLength = this.value;
+            saveSettings_icigqyni();
+        });
+    }
+
+    function saveSettings_icigqyni() {
+        let file = JSON.parse(localStorage.getItem("tasselSettings2") || "{}");
+        file.readThis = settings;
+        localStorage.setItem("tasselSettings2", JSON.stringify(file));
     }
 })();
