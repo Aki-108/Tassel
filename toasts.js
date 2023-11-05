7703065150778350e5a7f85b7430f24efa7e771b
Aki <75360508+Aki-108@users.noreply.github.com>
2023-11-05 11:03:19 +0100

    Update toasts.js

--- toasts.js
+++ toasts.js
@@ -8,43 +8,19 @@ let toasts = [
     "new": false
   },
   {
-    "id": 14,
-    "extension": 9,
-    "title": "New Extension",
-    "text": "Post Charts is now available for activation",
-    "timestamp": 1693639280661,
-    "new": true
-  },
-  {
-    "id": 15,
-    "extension": 8,
-    "title": "Pausable GIFs",
-    "text": "The extension 'Image Censor' now has the option to stop GIF auto-play.",
-    "timestamp": 1694245433855,
-    "new": false
-  },
-  {
-    "id": 16,
-    "extension": 10,
-    "title": "New Extension",
-    "text": "Sidebar Counts is now available as a work-in-progress extension.",
-    "timestamp": 1694995080000,
-    "new": true
-  },
-  {
-    "id": 17,
-    "extension": 11,
-    "title": "New: Read This",
-    "text": "The new extension, 'Read This', let's you open Read-More's without opening the post in a new tab. And it can shorten long posts.",
-    "timestamp": 1696017952450,
-    "new": true
-  },
-  {
     "id": 18,
     "extension": 12,
     "title": "WIP: Fort Archive",
     "text": "'Fort Archive' let's you see many posts at once and jump to a specific time. But it is still work-in-progress.",
     "timestamp": 1696333244573,
+    "new": true
+  },
+  {
+    "id": 19,
+    "extension": 13,
+    "title": "New Extension",
+    "text": "The setting 'Hide 0 Notification Counter' has been replaced with the extension 'Hide Numbers'.",
+    "timestamp": 1699174060526,
     "new": true
   }
 ];
