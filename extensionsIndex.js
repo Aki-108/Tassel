a34c467821c93de5e47a8c1c0d9b65b24e0d792e
Aki <75360508+Aki-108@users.noreply.github.com>
2024-02-17 08:04:22 +0100

    Updated Time Format

--- extensionsIndex.js
+++ extensionsIndex.js
@@ -218,10 +218,10 @@ let extensionsIndex = [
                 "id": 16,
                 "version": "0.1",
                 "created": 1708117680669,
-                "updated": 1708117680669,
+                "updated": 1708153448455,
                 "author": "Aki108",
                 "description": "Format timestamps any way you want.",
-                "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@0e415aac70453777943ad6e11e4b30f95f0f9e91/extensions/TimeFormat/TimeFormat.js",
+                "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@699e2afec22bf57a39cd51031e08977ac53bf453/extensions/TimeFormat/TimeFormat.js",
                 "css": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@86afcba701f5d9deca1a69cb7b3c394f8d256752/extensions/TimeFormat/TimeFormat.css",
                 "post": "https://www.pillowfort.social/posts/4348164"
         }
