3b62307cf98fb193fe16bca8ac828161a88c8f07
Aki <75360508+Aki-108@users.noreply.github.com>
2024-03-02 07:12:36 +0100

    Updated Tagging Tools Styling

--- extensionsIndex.js
+++ extensionsIndex.js
@@ -93,7 +93,7 @@ let extensionsIndex = [
             "author": "Aki108",
             "description": "Makes consistent tagging easier.",
             "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@2184ab1af9b7e504f6e7653c31c142df0cde81e5/extensions/TaggingTools/TaggingTools.js",
-            "css": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@6633a9b7438bdfe18107716f0068cafde33eb53d/extensions/TaggingTools/TaggingTools.css",
+            "css": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@9c86459f111b1953d17ccd297f544821613d3dac/extensions/TaggingTools/TaggingTools.css",
             "post": "https://www.pillowfort.social/posts/3228885",
             "features": [
                 "Suggests tags based on your previous posts.",
