070a4d9e1f73c44bf499d943161c9056b7fa5a68
Aki <75360508+Aki-108@users.noreply.github.com>
2024-03-03 18:46:48 +0100

    Fix for Modal

--- jsonManager.js
+++ jsonManager.js
@@ -73,7 +73,11 @@ function initModal_quugasdg() {
 
     let modalObserver = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutationRecord) {
-            if (mutationRecord.attributeName === "href") {
+            if (document.getElementById("reblog-modal")) {
+                tasselJsonManager.modal = tasselJsonManager.post;
+                assignUsers_quugasdg(tasselJsonManager.modal.json);
+                trigger_quugasdg("tasselJsonManagerModalReady");
+            } else if (mutationRecord.attributeName === "href") {
                 let postId = mutationRecord.target.href;
                 postId = postId.substring(postId.search("/posts/")+7);
                 tasselJsonManager.modal.postId = postId;
@@ -89,10 +93,6 @@ function initModal_quugasdg() {
                     tasselJsonManager.modal.ready = false;
                     tasselJsonManager.comments.ready = false;
                 }
-            } else if (document.getElementById("reblog-modal")) {
-                tasselJsonManager.modal = tasselJsonManager.post;
-                assignUsers_quugasdg(tasselJsonManager.modal.json);
-                trigger_quugasdg("tasselJsonManagerModalReady");
             }
         });
     });
