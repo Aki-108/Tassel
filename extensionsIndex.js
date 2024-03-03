b151ad707b6c346c9da33f725af50b4c66b0eeb3
Aki <75360508+Aki-108@users.noreply.github.com>
2024-03-03 11:43:33 +0100

    Updated TT Feature List

--- extensionsIndex.js
+++ extensionsIndex.js
@@ -98,7 +98,8 @@ let extensionsIndex = [
             "features": [
                 "Suggests tags based on your previous posts.",
                 "Adds a button for copying tags when reblogging.",
-                "Set default tags that automatically apply to posts you make/reblog."
+                "Set default tags that automatically apply to posts you make/reblog.",
+                "View the rules of a community while reblogging to it.
             ]
         },
         {
