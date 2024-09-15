243ad1924f64f1aec197481ac6369ba45da91f4a
Aki <75360508+Aki-108@users.noreply.github.com>
2024-09-15 17:19:49 +0200

    Create ForAccessibility.js

---
+++ extensions/ForAccessibility.js
@@ -0,0 +1,94 @@
+// ==UserScript==
+// @name         For Accessibility
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Accessibility Tools
+// @author       Aki108
+// @match        https://www.pillowfort.social/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pillowfort.social
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let editorObserver = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutationRecord) {
+            mutationRecord.addedNodes.forEach(function(node) {
+                if (node.tagName === "IMG" && node.id.length === 0) {
+                    let id = "tasselForAccessiblityImage" + Math.random();
+                    let altInput = document.createElement("textarea");
+                    altInput.classList.add("tasselForAccessiblityEditorInput");
+                    altInput.setAttribute("image", id);
+                    altInput.placeholder = "alt text";
+                    altInput.addEventListener("keyup", function() {
+                        document.getElementById(this.getAttribute("image")).alt = this.value;
+                    });
+                    console.log(altInput);
+                    node.after(altInput);
+                    node.id = id;
+                    console.log(node);
+                } else {
+                    let altInputs = Object.values(document.getElementsByClassName("tasselForAccessiblityEditorInput"));
+                    altInputs.forEach(function(input) {
+                        input.value = document.getElementById(input.getAttribute("image")).alt;
+                        input.addEventListener("keyup", function() {
+                            document.getElementById(this.getAttribute("image")).alt = this.value;
+                        });
+                    });
+                }
+            });
+        });
+    });
+
+    window.setTimeout(init_pdmmlnvi, 0);
+    function init_pdmmlnvi() {
+        let editor = document.getElementsByClassName("main","create-post-main");
+        if (editor.length > 0) {
+            editor = editor[0].getElementsByClassName("fr-element","fr-view");
+            if (editor.length > 0) {
+                editor = editor[0];
+                console.log(document.getElementById("post_content").value);
+                $.get(document.URL, function(data) {
+                    console.log("data fetched!!!");
+                    let html = document.createElement("body");
+                    html.innerHTML = data.substring(data.search("<body>")+6, data.search("</body>"));
+                    let content = html.children[21].children[0].children[3].children[0].children[1].value;
+                    editor.innerHTML = content;
+                })
+                .fail(function() {
+                    console.log("failed");
+                });
+                editorObserver.observe(editor, {
+                    childList: true,
+                    subtree: true
+                });
+            }
+        }
+
+        document.getElementById("tasselJsonManagerPostReady").addEventListener("click", function() {
+            let imgs = Object.values(document.getElementsByTagName("img"));
+            imgs = imgs.filter(function(img) {
+                return img.alt.length > 0 && !img.classList.contains("svg-purple-dark");
+            });
+            imgs.forEach(function(img) {
+                let altText = document.createElement("p");
+                altText.style = "background: var(--tag_bg); padding: 0.5em;";
+                altText.innerHTML = "Alt: " + img.alt;
+                img.after(altText);
+            });
+        });
+
+        document.getElementById("tasselJsonManagerFeedReady").addEventListener("click", function() {
+            let imgs = Object.values(document.getElementsByTagName("img"));
+            imgs = imgs.filter(function(img) {
+                return img.alt.length > 0;
+            });
+            imgs.forEach(function(img) {
+                let altText = document.createElement("p");
+                altText.style = "background: var(--tag_bg); padding: 0.5em;";
+                altText.innerHTML = "Alt: " + img.alt;
+                img.after(altText);
+            });
+        });
+    }
+})();
