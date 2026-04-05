8249e863fa190984f8e16898feac77289ec4e1f0
Aki <75360508+Aki-108@users.noreply.github.com>
2026-04-05 09:24:45 +0200

    Updated Added Accessiblity

--- extensionsIndex.js
+++ extensionsIndex.js
@@ -243,7 +243,7 @@ let extensionsIndex = [
                 "updated": 1775296086076,
                 "author": "Aki108",
                 "description": "Changes to Alt Text.",
-                "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@1722f3be1c5fa74a628afa49d0d56a873653a6e5/extensions/AddedAccessibility/AddedAccessibility.js",
+                "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@b758f09ab12a3be7f20a7b19bec20618bcdf9032/extensions/AddedAccessibility/AddedAccessibility.js",
                 "css": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@1a2c16a6b1ad53367e1b8c46e2dc00311d4f9b2d/extensions/AddedAccessibility/AddedAccessibility.css",
                 "post": "https://www.pillowfort.social/posts/5332841"
         },
