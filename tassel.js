1d17b7ec578689ab904796c8890f6926981e700f
Aki <75360508+Aki-108@users.noreply.github.com>
2026-04-19 21:47:06 +0200

    Update tassel.js

--- tassel.js
+++ tassel.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Tassel
-// @version      1.8.3
+// @version      1.8.4
 // @description  Pillowfort Extension Manager. Makes the use of a variety of extensions easier.
 // @author       Aki108
 // @match        https://www.pillowfort.social/*
@@ -14,9 +14,9 @@
 (function() {
     'use strict';
 
-    let extensionsIndexURL = "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@5c6a708948799ba162455482bc29537a30ba58ac/extensionsIndex.js";
+    let extensionsIndexURL = "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@c6a65d9c88de7d7526a883fb2cfffe3dc9053a6b/extensionsIndex.js";
     let toastsURL = "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@5716332e94d08b1a0662a799ac2dba905f8f1f11/toasts.js";
-    let styleURL = "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@ac9b1ad615f4b72a9a1cde27e4d90c508c9936c0/style.css";
+    let styleURL = "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@224bf36292bfc56dfdf121ece01fcfdf0dd5b029/style.css";
     let jsonManager = "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@1bf28bff4356ad6503fcd146403b38c117051e97/jsonManager.js";
 
     let icon = document.createElement("div");
