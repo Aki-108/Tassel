b0a8312d8110c3803aade8a610fa329b7cdb2168
Aki <75360508+Aki-108@users.noreply.github.com>
2026-09-06 14:46:33 +0200

    Updated FortArchive

--- extensionsIndex.js
+++ extensionsIndex.js
@@ -178,7 +178,7 @@ let extensionsIndex = [
                 "updated": 1788682896993,
                 "author": "Aki108",
                 "description": "See many posts at once.",
-                "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@6a0ec29401820de373f5bb42ca67c82ec9c9b654/extensions/FortArchive/FortArchive.js",
+                "src": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@10de88f12b1966837c8f75b7a87aefcf4aed0526/extensions/FortArchive/FortArchive.js",
                 "css": "https://cdn.jsdelivr.net/gh/Aki-108/Tassel@7f64a859ac233305b7090ee4a879ee909c364abc/extensions/FortArchive/FortArchive.css",
                 "post": "https://www.pillowfort.social/posts/3798470",
                 "features": [
